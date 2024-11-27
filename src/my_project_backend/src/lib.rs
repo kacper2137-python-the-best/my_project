fdb960a4e53bf64810c7c1c57c223c138b190087
kacper Barszcz <u152_kacbar_waw@technischools.com>
2024-11-27 10:03:06 +0000

    Created messeg story

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,19 @@
+usestd::call::RefCall
+
+thread_local! {
+    static MSG: RefCall<String> = RefCall::new(String::new());
+}
+#[ic_cdk::update]
+fn sve_msg(msg:String)
+MSG.widht([static_msg] *static_msg.borrow_mut() = msg);
+
 #[ic_cdk::query]
+fn get_msg() -> String {
+    msg.with([static_msg] *static_msg.borrow_mut().clone())
+}
+
+
+#[ic_cdk::query]"sta"
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
